b76b35e3315db5a5f4ccdc9b56ec61e36fa79b1f
LuisaP <luisa.pegoraro@gmail.com>
2019-07-10 15:21:58 -0400

    first commit

--- server.js
+++ server.js
@@ -4,14 +4,20 @@ const app = express();
 const PORT = process.env.port || 3000; 
 
 app.use(express.static(__dirname + "/public"));  
-app.use(express.static(__dirname + "/views"));  
+console.log(path.join(__dirname + "/public/index.html"))
+// app.use(express.static(__dirname + "/views"));  
+
+app.get("/", function(req, res){  
+    res.sendFile(path.join(__dirname + "/public/index.html")); 
+});
 
 app.post("/gethours", function(req, res){
     var a = {"MF": "6:00AM - 3:00PM", "WH": "7:00AM - 2:00PM"};
     res.status(200).send(a);
-    // console.log("Hello there");
     console.log(a);
-})
+});
+
+
 
 // app.get("/partnership", function(req, res){  
 //     res.sendFile(path.join(__dirname + "/views/partnership.html")); // This is not working
