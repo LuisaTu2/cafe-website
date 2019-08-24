5529c5dbd7c5c0301343f6719b1892d73f11a5e1
LuisaP <luisa.pegoraro@gmail.com>
2019-08-24 17:31:02 -0400

    Removed node_module folder

--- node_modules/vue2-google-maps/dist/components/autocomplete.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <input type="text" :placeholder="placeholder" ref="input"
-    :value="value"/>
-</template>
-
-<script>
-export default (function (x) { return x.default || x })(require('./autocompleteImpl.js'))
-</script>
